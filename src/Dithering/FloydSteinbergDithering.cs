921034ed9afb416b400fd6a740d733f4569c1dd3
Richard Moss <richard.moss@cyotek.com>
2015-08-09 09:20:08 +0100

    Changed FloydSteinbergDithering to inherit ErrorDiffusionDithering
    The original FloydSteinbergDithering class implemented all the logic
    itself. Now inherits from ErrorDiffusionDithering and so lets that
    handle the logic.

--- src/Dithering/FloydSteinbergDithering.cs
+++ src/Dithering/FloydSteinbergDithering.cs
@@ -6,83 +6,26 @@
  * Licensed under the MIT License. See LICENSE.txt for the full text.
  */
 
+using System.ComponentModel;
+
 namespace Cyotek.Drawing.Imaging.ColorReduction
 {
-  public sealed class FloydSteinbergDithering : IErrorDiffusion
+  [Description("Floyd-Steinberg")]
+  public sealed class FloydSteinbergDithering : ErrorDiffusionDithering
   {
-    #region IErrorDiffusion Interface
+    #region Constructors
 
-    void IErrorDiffusion.Diffuse(ArgbColor[] data, ArgbColor original, ArgbColor transformed, int x, int y, int width, int height)
-    {
-      ArgbColor offsetPixel;
-      int redError;
-      int blueError;
-      int greenError;
-      int offsetIndex;
-      int index;
-
-      /*
-        * Floyd-Steinberg Dithering
-        * http://en.wikipedia.org/wiki/Floyd%E2%80%93Steinberg_dithering
-        *
-        * Coefficients for dispersing the error (* is the current pixel):
-        *
-        *             *  7/16
-        *      3/16 5/16 1/16
-        *
-        * As 16 is a power of two, we can use bit shifting to do the
-        * division which is actually faster than the division operator
-        */
-
-      index = y * width + x;
-      redError = original.R - transformed.R;
-      blueError = original.G - transformed.G;
-      greenError = original.B - transformed.B;
-
-      if (x + 1 < width)
-      {
-        // right
-        offsetIndex = index + 1;
-        offsetPixel = data[offsetIndex];
-        offsetPixel.R = (offsetPixel.R + ((redError * 7) >> 4)).ToByte();
-        offsetPixel.G = (offsetPixel.G + ((greenError * 7) >> 4)).ToByte();
-        offsetPixel.B = (offsetPixel.B + ((blueError * 7) >> 4)).ToByte();
-        data[offsetIndex] = offsetPixel;
-      }
-
-      if (y + 1 < height)
-      {
-        if (x - 1 > 0)
-        {
-          // left and down
-          offsetIndex = index + width - 1;
-          offsetPixel = data[offsetIndex];
-          offsetPixel.R = (offsetPixel.R + ((redError * 3) >> 4)).ToByte();
-          offsetPixel.G = (offsetPixel.G + ((greenError * 3) >> 4)).ToByte();
-          offsetPixel.B = (offsetPixel.B + ((blueError * 3) >> 4)).ToByte();
-          data[offsetIndex] = offsetPixel;
-        }
-
-        // down
-        offsetIndex = index + width;
-        offsetPixel = data[offsetIndex];
-        offsetPixel.R = (offsetPixel.R + ((redError * 5) >> 4)).ToByte();
-        offsetPixel.G = (offsetPixel.G + ((greenError * 5) >> 4)).ToByte();
-        offsetPixel.B = (offsetPixel.B + ((blueError * 5) >> 4)).ToByte();
-        data[offsetIndex] = offsetPixel;
-
-        if (x + 1 < width)
-        {
-          // right and down
-          offsetIndex = index + width + 1;
-          offsetPixel = data[offsetIndex];
-          offsetPixel.R = (offsetPixel.R + ((redError * 1) >> 4)).ToByte();
-          offsetPixel.G = (offsetPixel.G + ((greenError * 1) >> 4)).ToByte();
-          offsetPixel.B = (offsetPixel.B + ((blueError * 1) >> 4)).ToByte();
-          data[offsetIndex] = offsetPixel;
-        }
-      }
-    }
+    public FloydSteinbergDithering()
+      : base(new byte[,]
+             {
+               {
+                 0, 0, 7
+               },
+               {
+                 3, 5, 1
+               }
+             }, 4, true)
+    { }
 
     #endregion
   }
